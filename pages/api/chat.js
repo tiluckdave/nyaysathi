24d1cf6f4e326cb9b6c822abbde2dd9bcf511177
tiluckdave <davetilak003@gmail.com>
2023-10-08 09:42:06 +0530

    base model

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -12,7 +12,7 @@ const generateAction = async (req, res) => {
     console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
 
     const baseCompletion = await openai.createCompletion({
-        model: 'text-davinci-003',
+        model: 'babbage-002',
         prompt: `${basePromptPrefix}${req.body.userInput}\n`,
         temperature: 0.6,
         max_tokens: 550,
