6e6d35a7103203137cd9295f67526b2d9af1fb93
tiluckdave <davetilak003@gmail.com>
2023-12-08 22:50:14 +0530

    file summarize

---
+++ pages/file-summarize.js
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+
+export default function FileSummarize() {
+    // save the file in directory 
+    const [file, setFile] = useState(null);
+    const [summary, setSummary] = useState(null);
+    const onFileChanged = (event) => {
+        setFile(event.target.files[0]);
+    };
+
+    // send the file to flask server
+    const uploadFile = () => {
+        const formData = new FormData();
+        formData.append("file", file);
+
+        fetch("http://localhost:5000/summarize", {
+            method: "POST",
+            body: formData,
+        })
+            .then((response) => response.json())
+            .then((result) => {
+                setSummary(result.summary);
+            })
+            .catch((error) => {
+                console.error("Error:", error);
+            });
+    };
+
+    return (
+        <div>
+            <h1>File Summarize</h1>
+            <input type="file" onChange={onFileChanged} />
+            <button onClick={uploadFile}>Summarize</button>
+
+            <div>
+                {summary}
+            </div>
+        </div>
+    )
+}
