901931aa20182a9efd47ebbf0714d629f5309c1c
tiluckdave <davetilak003@gmail.com>
2023-12-17 22:18:45 +0530

    pull karo guys

--- pages/lawyer/dashboard.js
+++ pages/lawyer/dashboard.js
@@ -1,7 +1,10 @@
 import LawyerDashboardWrapper from "@/components/LawyerDashboardWrapper";
 import { Heading } from "@chakra-ui/react";
+import { UserAuth } from "@/lib/auth";
 
 export default function Dashboard() {
+    const { user } = UserAuth();
+
     return (
         <LawyerDashboardWrapper page="dashboard">
             <Heading fontSize="3xl">Lawyer Dashboard</Heading>
